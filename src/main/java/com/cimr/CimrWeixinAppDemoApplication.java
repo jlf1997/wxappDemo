a3b5299d0fe14244cc733df331cc7d014524520c
yangxiang <675866753@qq.com>
2018-06-11 10:30:36 +0800

    new project

---
+++ src/main/java/com/cimr/CimrWeixinAppDemoApplication.java
@@ -0,0 +1,12 @@
+package com.cimr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CimrWeixinAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CimrWeixinAppDemoApplication.class, args);
+	}
+}
